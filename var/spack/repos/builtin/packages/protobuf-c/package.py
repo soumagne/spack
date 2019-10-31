f8802827d5d39317f101a4f133316ecb90dcd005
Jerome Soumagne <jsoumagne@hdfgroup.org>
2019-10-31 16:53:12 -0500

    Fixes for protobuf-c

--- var/spack/repos/builtin/packages/protobuf-c/package.py
+++ var/spack/repos/builtin/packages/protobuf-c/package.py
@@ -13,8 +13,26 @@ class ProtobufC(AutotoolsPackage):
 
     homepage = "https://github.com/protobuf-c/protobuf-c"
     url      = "https://github.com/protobuf-c/protobuf-c/releases/download/v1.3.2/protobuf-c-1.3.2.tar.gz"
+    git      = "https://github.com/protobuf-c/protobuf-c.git"
 
+    version('master', branch='master')
+
+    # Current
+    version('1.3.3', sha256='22956606ef50c60de1fabc13a78fbc50830a0447d780467d3c519f84ad527e78')
     version('1.3.2', sha256='53f251f14c597bdb087aecf0b63630f434d73f5a10fc1ac545073597535b9e74')
+    version('1.3.1', sha256='51472d3a191d6d7b425e32b612e477c06f73fe23e07f6a6a839b11808e9d2267')
 
+    depends_on('autoconf', type='build', when='@master')
+    depends_on('automake', type='build', when='@master')
+    depends_on('libtool',  type='build', when='@master')
+    depends_on('m4', type='build', when='@master')
     depends_on('protobuf')
     depends_on('pkgconfig', type='build')
+
+    def configure_args(self):
+        spec = self.spec
+        config_args = [
+            '--enable-shared',
+        ]
+
+        return config_args
